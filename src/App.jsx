c34b78b3f2fa7880ba2fee62baf548104241d292
Harsh Jagtap <harshjagtap843@gmail.com>
2026-02-03 01:27:50 +0530

    final done

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,13 @@
 import React from 'react'
 import './App.css'
-
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 const App = () => {
   return (
     <div>
-      <nav className='flex max-w-[1280px] justify-between mx-auto h-[72px] items-center px-5'>
-
-        <div className='logo' >
-          <img src="/images/brand_logo.png" alt="logo" />
-        </div>
-
-        <ul className='flex w-[333px] h-[24px] gap-[24px]' >
-          <li href='#' >MENU</li>
-          <li href='#'>LOCATION</li>
-          <li href='#'>ABOUT</li>
-          <li href='#'>CONTACT</li>
-        </ul>
-
-          <button className='w-[75px] h-[40px] px-[16px] py-[6px] bg-[#D01C28] text-white font-medium' >
-            Login
-          </button>
-
-
-      </nav>
+      <Navbar/>
+      <Hero/>
     </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+            <nav className='flex max-w-[1280px] justify-between mx-auto h-[72px] items-center px-5'>
+
+                <div className='logo' >
+                    <img src="/images/brand_logo.png" alt="logo" />
+                </div>
+ 
+                <ul className='flex w-[333px] h-[24px] gap-[24px]' >
+                    <li href='#'>MENU</li>
+                    <li href='#'>LOCATION</li>
+                    <li href='#'>ABOUT</li>
+                    <li href='#'>CONTACT</li>
+                </ul>
+
+                <button className='w-[75px] h-[40px] px-[16px] py-[6px] bg-[#D01C28] text-white font-medium' >
+                    Login
+                </button>
+
+
+            </nav>
+    )
+}
+
+export default Navbar
